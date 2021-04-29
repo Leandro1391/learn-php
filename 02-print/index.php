cba2c65211d172f2ee0ef3ed5da2001dcc662003
Leandro Julian <leandrojulian1391@gmail.com>
2021-04-28 22:04:03 -0300

    comment line and block

---
+++ 02-print/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>My firts PHP Inyection in html</h1>
+
+    <?='Welcome to my page in PHP'?>
+
+    <?php
+        // Esto es un comentario
+
+        /* echo "<h3>Listado de videojuegos</h3>";
+        echo "<ul>"
+            ."<li>GTA</li>"
+            ,"<li>Fifa</li>"
+            ,"<li>Mario Bros</li>"
+            ,"</ul>"; */
+
+        /* echo "<br>Hello hello hello<br>"; */
+
+        // Parrafo explicativo
+        echo '<p>Esta es toda'.' - '.'lista de juegos </p>';
+
+    ?>
+</body>
+</html>
+
